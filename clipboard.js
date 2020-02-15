0ecff0857ca3a1dced334a976134ee49c4b74ae8
leeeso93 <leeeso93@gmail.com>
2020-02-15 14:03:09 +0900

    cliboard 강의에서 들은 코드로 바꿈

--- clipboard.js
+++ clipboard.js
@@ -3,10 +3,6 @@
  * All rights reserved. 무단전재 및 재배포 금지.
  * All contents cannot be copied without permission.
  */
-
-/**
- * 프로필 관련 정보를 불러옴
- */
 (async () => {
   const common = (() => {
     const IMG_PATH = "https://it-crafts.github.io/lesson/img";
@@ -45,14 +41,14 @@
 
     const render = () => {
       $parent.innerHTML = `
-            <div class="v9tJq">
-                <div class="fx7hk">
-                    <a class="_9VEo1 T-jvg" href="timeline.htm" data-type="grid"><span aria-label="게시물" class="glyphsSpritePhoto_grid__outline__24__grey_5 u-__7"></span></a>
-                    <a class="_9VEo1" href="javascript:;" data-type="feed"><span aria-label="피드" class="glyphsSpritePhoto_list__outline__24__grey_5 u-__7"></span></a>
-                    <a class="_9VEo1" href="clipboard.htm" data-type=""><span aria-label="태그됨" class="glyphsSpriteTag_up__outline__24__blue_5 u-__7"></span></a>
-                </div>
-            </div>
-        `;
+              <div class="v9tJq">
+                  <div class="fx7hk">
+                      <a class="_9VEo1 T-jvg" href="javascript:;" data-type="grid"><span aria-label="게시물" class="glyphsSpritePhoto_grid__outline__24__grey_5 u-__7"></span></a>
+                      <a class="_9VEo1" href="javascript:;" data-type="feed"><span aria-label="피드" class="glyphsSpritePhoto_list__outline__24__grey_5 u-__7"></span></a>
+                      <a class="_9VEo1" href="javascript:;" data-type=""><span aria-label="태그됨" class="glyphsSpriteTag_up__outline__24__blue_5 u-__7"></span></a>
+                  </div>
+              </div>
+          `;
     };
 
     create();
@@ -82,54 +78,54 @@
       $parent.insertAdjacentHTML(
         "afterbegin",
         `
-            <div>
-                <header class="HVbuG">
-                    <div class="XjzKX">
-                        <div class="RR-M- h5uC0" role="button" tabindex="0">
-                            <canvas class="CfWVH" height="91" width="91" style="position: absolute; top: -7px; left: -7px; width: 91px; height: 91px;"></canvas>
-                            <span class="_2dbep" role="link" tabindex="0" style="width: 77px; height: 77px;"><img alt="${
-                              data.name
-                            }님의 프로필 사진" class="_6q-tv" src="${
+              <div>
+                  <header class="HVbuG">
+                      <div class="XjzKX">
+                          <div class="RR-M- h5uC0" role="button" tabindex="0">
+                              <canvas class="CfWVH" height="91" width="91" style="position: absolute; top: -7px; left: -7px; width: 91px; height: 91px;"></canvas>
+                              <span class="_2dbep" role="link" tabindex="0" style="width: 77px; height: 77px;"><img alt="${
+                                data.name
+                              }님의 프로필 사진" class="_6q-tv" src="${
           common.IMG_PATH
         }${data.img}"></span>
-                        </div>
-                    </div>
-                    <section class="zwlfE">
-                        <div class="nZSzR">
-                            <h1 class="_7UhW9 fKFbl yUEEX KV-D4 fDxYl">${
-                              data.name
-                            }</h1>
-                            <span class="mrEK_ Szr5J coreSpriteVerifiedBadge" title="인증됨">인증됨</span>
-                            <div class="AFWDX"><button class="dCJp8 afkep"><span aria-label="옵션" class="glyphsSpriteMore_horizontal__outline__24__grey_9 u-__7"></span></button></div>
-                        </div>
-                        <div class="Y2E37">
-                            <div class="Igw0E IwRSH eGOV_ vwCYk">
-                                <span class="ffKix bqE32">
-                                    <span class="vBF20 _1OSdk"><button class="_5f5mN jIbKX _6VtSN yZn4P">팔로우</button></span>
-                                    <span class="mLCHD _1OSdk"><button class="_5f5mN jIbKX KUBKM yZn4P"><div class="OfoBO"><div class="_5fEvj coreSpriteDropdownArrowWhite"></div></div></button></span>
-                                </span>
-                            </div>
-                        </div>
-                    </section>
-                </header>
-                <div class="-vDIg">
-                    <h1 class="rhpdm">${data.title}</h1><br><span>${
+                          </div>
+                      </div>
+                      <section class="zwlfE">
+                          <div class="nZSzR">
+                              <h1 class="_7UhW9 fKFbl yUEEX KV-D4 fDxYl">${
+                                data.name
+                              }</h1>
+                              <span class="mrEK_ Szr5J coreSpriteVerifiedBadge" title="인증됨">인증됨</span>
+                              <div class="AFWDX"><button class="dCJp8 afkep"><span aria-label="옵션" class="glyphsSpriteMore_horizontal__outline__24__grey_9 u-__7"></span></button></div>
+                          </div>
+                          <div class="Y2E37">
+                              <div class="Igw0E IwRSH eGOV_ vwCYk">
+                                  <span class="ffKix bqE32">
+                                      <span class="vBF20 _1OSdk"><button class="_5f5mN jIbKX _6VtSN yZn4P">팔로우</button></span>
+                                      <span class="mLCHD _1OSdk"><button class="_5f5mN jIbKX KUBKM yZn4P"><div class="OfoBO"><div class="_5fEvj coreSpriteDropdownArrowWhite"></div></div></button></span>
+                                  </span>
+                              </div>
+                          </div>
+                      </section>
+                  </header>
+                  <div class="-vDIg">
+                      <h1 class="rhpdm">${data.title}</h1><br><span>${
           data.text
         }</span>
-                </div>
-                <ul class="_3dEHb">
-                    <li class="LH36I"><span class="_81NM2">게시물 <span class="g47SY lOXF2">${
-                      data.post
-                    }</span></span></li>
-                    <li class="LH36I"><a class="_81NM2" href="javascript:;">팔로워 <span class="g47SY lOXF2" title="${
-                      data.follower
-                    }">${scaleDown(data.follower)}</span></a></li>
-                    <li class="LH36I"><a class="_81NM2" href="javascript:;">팔로우 <span class="g47SY lOXF2">${
-                      data.follow
-                    }</span></a></li>
-                </ul>
-            </div>
-        `
+                  </div>
+                  <ul class="_3dEHb">
+                      <li class="LH36I"><span class="_81NM2">게시물 <span class="g47SY lOXF2">${
+                        data.post
+                      }</span></span></li>
+                      <li class="LH36I"><a class="_81NM2" href="javascript:;">팔로워 <span class="g47SY lOXF2" title="${
+                        data.follower
+                      }">${scaleDown(data.follower)}</span></a></li>
+                      <li class="LH36I"><a class="_81NM2" href="javascript:;">팔로우 <span class="g47SY lOXF2">${
+                        data.follow
+                      }</span></a></li>
+                  </ul>
+              </div>
+          `
       );
     };
 
@@ -149,11 +145,11 @@
       $parent.insertAdjacentHTML(
         "beforeend",
         `
-            <div class="_2z6nI">
-                <div style="flex-direction: column;">
-                </div>
-            </div>
-        `
+              <div class="_2z6nI">
+                  <div style="flex-direction: column;">
+                  </div>
+              </div>
+          `
       );
     };
 
@@ -165,11 +161,59 @@
     let $el;
 
     let page = 1;
+    const ITEM_PER_ROW = 3;
     const timelineList = await common.fetchApiData(url, page++);
+    let keyword = "";
 
     const create = () => {
       render();
       $el = $parent.lastElementChild;
+      addEvent();
+    };
+
+    // addEventListener 함수
+    const addEvent = () => {
+      $el.firstElementChild.lastElementChild
+        .querySelector("input")
+        .addEventListener("keyup", keyup);
+      $el.firstElementChild.addEventListener("click", click);
+    };
+
+    const keyup = function(e) {
+      keyword = this.value;
+      $el.lastElementChild.firstElementChild.innerHTML = "";
+      renderGridItem(divide(filter(timelineList), ITEM_PER_ROW)); // 리랜더링
+    };
+
+    const click = function(e) {
+      const kind = e.target.dataset.kind;
+      console.log(kind);
+
+      if (!kind) {
+        return;
+      }
+      $el.lastElementChild.firstElementChild.innerHTML = "";
+      renderGridItem(divide(sort(kind), ITEM_PER_ROW)); // 리랜더링
+    };
+
+    const filter = list => {
+      if (!keyword) {
+        return list;
+      }
+      return list.filter(i => (i.text + i.name).includes(keyword));
+    };
+
+    const comparator = {
+      popular: (a, b) =>
+        b.clipCount * 1 +
+        b.commentCount * 2 -
+        a.clipCount * 1 +
+        a.commentCount * 2,
+      lastest: (a, b) => Date.parse(b.timestamp) - Date.parse(a.timestamp)
+    };
+    const sort = kind => {
+      timelineList.sort(comparator[kind]);
+      return filter(timelineList);
     };
 
     const divide = (list, size) => {
@@ -181,147 +225,102 @@
         listList.push(copy.splice(0, size));
       }
 
-      const lastlist = listList[listList.length - 1];
-      console.log(lastlist);
-      for (let i = lastlist.length; i < size; i++) {
-        lastlist[i] = {};
+      if (listList.length !== 0) {
+        const lastlist = listList[listList.length - 1];
+        for (let i = lastlist.length; i < size; i++) {
+          lastlist[i] = {};
+        }
       }
 
       return listList;
     };
-    const listList = divide(timelineList, 3);
+    const listList = divide(timelineList, ITEM_PER_ROW);
 
     const render = () => {
       $parent.insertAdjacentHTML(
         "beforeend",
         `
-            <article class="FyNDV">
-                <div class="Igw0E rBNOH YBx95 ybXk5 _4EzTm soMvl JI_ht bkEs3 DhRcB">
-                    <button class="sqdOP L3NKy y3zKF JI_ht" type="button" id="recentSortBtn">최신순</button>
-                    <button class="sqdOP L3NKy y3zKF JI_ht" type="button" id="popularSortBtn">인기순</button>
-                    <h1 class="K3Sf1">
-                        <div class="Igw0E rBNOH eGOV_ ybXk5 _4EzTm">
-                            <div class="Igw0E IwRSH eGOV_ vwCYk">
-                                <div class="Igw0E IwRSH eGOV_ ybXk5 _4EzTm">
-                                    <div class="Igw0E IwRSH eGOV_ vwCYk">
-                                        <label class="NcCcD">
-                                            <input autocapitalize="none"
-                                            autocomplete="off" class="j_2Hd iwQA6 RO68f M5V28" placeholder="검색" id="searchInput" spellcheck="true" type="search" value="" />
-                                            <div class="DWAFP">
-                                                <div class="Igw0E IwRSH eGOV_ _4EzTm">
-                                                    <span aria-label="검색" class="glyphsSpriteSearch u-__7"></span>
-                                                </div>
-                                                <span class="rwQu7">검색</span>
-                                            </div>
-                                            <div class="Igw0E rBNOH YBx95 _4EzTm ItkAi O1flK fm1AK TxciK yiMZG"></div>
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </h1>
-                </div>
-                <div>
-                    <div style="flex-direction: column; padding-bottom: 0px; padding-top: 0px;">
-                    </div>
-                </div>
-            </article>
-        `
+              <article class="FyNDV">
+                  <div class="Igw0E rBNOH YBx95 ybXk5 _4EzTm soMvl JI_ht bkEs3 DhRcB">
+                      <button data-kind="lastest" class="sqdOP L3NKy y3zKF JI_ht" type="button">최신순</button>
+                      <button data-kind="popular" class="sqdOP L3NKy y3zKF JI_ht" type="button">인기순</button>
+                      <h1 class="K3Sf1">
+                          <div class="Igw0E rBNOH eGOV_ ybXk5 _4EzTm">
+                              <div class="Igw0E IwRSH eGOV_ vwCYk">
+                                  <div class="Igw0E IwRSH eGOV_ ybXk5 _4EzTm">
+                                      <div class="Igw0E IwRSH eGOV_ vwCYk">
+                                          <label class="NcCcD">
+                                              <input autocapitalize="none" autocomplete="off" class="j_2Hd iwQA6 RO68f M5V28" placeholder="검색" spellcheck="true" type="search" value="" />
+                                              <div class="DWAFP">
+                                                  <div class="Igw0E IwRSH eGOV_ _4EzTm">
+                                                      <span aria-label="검색" class="glyphsSpriteSearch u-__7"></span>
+                                                  </div>
+                                                  <span class="rwQu7">검색</span>
+                                              </div>
+                                              <div class="Igw0E rBNOH YBx95 _4EzTm ItkAi O1flK fm1AK TxciK yiMZG"></div>
+                                          </label>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      </h1>
+                  </div>
+                  <div>
+                      <div style="flex-direction: column; padding-bottom: 0px; padding-top: 0px;">
+                      </div>
+                  </div>
+              </article>
+          `
       );
     };
 
-    //filter
-    const filter = event => {
-      // 현재는 각 컴포넌트가 destroy 미지원 -> 그냥 DOM만 비우고, 새로 gridItem들 생성
-      $el.lastElementChild.firstElementChild.innerHTML = "";
-
-      let searchValue = event.target.value;
-
-      const searchList = timelineList.filter(item => {
-        return item.text.indexOf(searchValue) !== -1 &&
-          item.name.indexOf(searchValue) !== -1;
-      });
-      if(searchList.length > 0) {
-        divide(searchList, 3);
-      }
-    };
-
-    //sort
-    const sort = event => {
-      // 현재는 각 컴포넌트가 destroy 미지원 -> 그냥 DOM만 비우고, 새로 gridItem들 생성
-      $el.lastElementChild.firstElementChild.innerHTML = "";
-      if (event.target.id === "recentSortBtn") {
-        // 최신순
-        timelineList.sort(
-          (x, y) => Date.parse(x.timestamp) - Date.parse(y.timestamp)
-        );
-      } else if (event.target.id === "popularSortBtn") {
-        //인기순
-        timelineList.sort(
-          (x, y) =>
-            x.clipCount +
-            x.commentCount * 2 -
-            (y.clipCount + y.commentCount * 2)
-        );
-      }
-      divide(timelineList, 3);
-    };
     create();
-
-    /**
-     * html에 이벤트 리스너 추가
-     */
-    const recentSortBtn = document.getElementById("recentSortBtn");
-    const popularSortBtn = document.getElementById("popularSortBtn");
-    const searchInput = document.getElementById("searchInput");
-
-    recentSortBtn.addEventListener("click", sort);
-    popularSortBtn.addEventListener("click", sort);
-    searchInput.addEventListener("keyup", filter);
-
     return { $el, listList };
   })(timelineContent.$el.firstElementChild, timeline.url);
 
-  grid.listList.forEach(list => {
-    let gridItem = (($parent, list) => {
-      let $el;
+  const renderGridItem = list => {
+    list.forEach(list => {
+      const gridItem = (($parent, list) => {
+        let $el;
 
-      const create = () => {
-        render(list);
-        $el = $parent.lastElementChild;
-      };
+        const create = () => {
+          render(list);
+          $el = $parent.lastElementChild;
+        };
 
-      const render = list => {
-        const html = list.reduce((html, data) => {
-          const img =
-            (data.img || "") &&
-            `
-                      <a href="javascript:;">
-                          <div class="eLAPa">
-                              <div class="KL4Bh">
-                                  <img class="FFVAD" decoding="auto" src="${common.IMG_PATH}${data.img}" style="object-fit: cover;">
-                              </div>
-                          </div>
-                      </a>
-                  `;
-          html += `
-                      <div class="v1Nh3 kIKUG _bz0w">${img}</div>
-                  `;
-          return html;
-        }, "");
-
-        $parent.insertAdjacentHTML(
-          "beforeend",
-          `
-                  <div class="Nnq7C weEfm">
-                      ${html}
-                  </div>
+        const render = list => {
+          const html = list.reduce((html, data) => {
+            const img =
+              (data.img || "") &&
               `
-        );
-      };
+                          <a href="javascript:;">
+                              <div class="eLAPa">
+                                  <div class="KL4Bh">
+                                      <img class="FFVAD" decoding="auto" src="${common.IMG_PATH}${data.img}" style="object-fit: cover;">
+                                  </div>
+                              </div>
+                          </a>
+                      `;
+            html += `
+                          <div class="v1Nh3 kIKUG _bz0w">${img}</div>
+                      `;
+            return html;
+          }, "");
 
-      create();
-      return { $el };
-    })(grid.$el.lastElementChild.firstElementChild, list);
-  });
+          $parent.insertAdjacentHTML(
+            "beforeend",
+            `
+                      <div class="Nnq7C weEfm">
+                          ${html}
+                      </div>
+                  `
+          );
+        };
+
+        create();
+        return { $el };
+      })(grid.$el.lastElementChild.firstElementChild, list);
+    });
+  };
+  renderGridItem(grid.listList);
 })();
